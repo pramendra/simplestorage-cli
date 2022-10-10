56af9a2e163e66e848c6ca8d67cc4ee9f9c782fc
Pramendra Gupta <pramendra@gmail.com>
2022-10-10 23:20:21 +0900

    updated env

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,9 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const NODE_ENV: string = process.env.NODE_ENV as string;
 yargs(hideBin(process.argv))
